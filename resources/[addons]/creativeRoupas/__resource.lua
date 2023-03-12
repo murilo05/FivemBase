0e322b3aabc0ecf91c328b734f9093eec532a768
murilo05 <itzfakeonalternative@outlook.com>
2023-03-12 04:45:56 -0300

    roupas

---
+++ resources/[addons]/creativeRoupas/__resource.lua
@@ -0,0 +1,11 @@
+--shared_script "@vrp/lib/lib.lua" --Para remover esta pendencia de todos scripts, execute no console o comando "uninstall"
+
+-- Created by myclothing
+
+files {
+    'peds.meta'
+}
+
+data_file 'PED_METADATA_FILE' 'peds.meta'
+fx_version 'cerulean'
+game { 'gta5' }
